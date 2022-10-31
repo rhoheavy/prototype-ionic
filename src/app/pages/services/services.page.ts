ad707aa38a5cba0d3551cb544f5b4510d767be26
Rena Salazar <renasalazart@outlook.com>
2022-10-30 21:21:55 -0700

    Pestaña de servicios y cards

--- src/app/pages/services/services.page.ts
+++ src/app/pages/services/services.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.page.html',
   styleUrls: ['./services.page.scss'],
 })
-export class ServicesPage implements OnInit {
+export class ServicesPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
